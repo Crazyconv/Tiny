e0afb665accd0f3dfceb37bc9038019bcf68e56f
Crazyconv <conanconv@163.com>
2014-09-21 23:40:15 -0700

    prepare for testing

--- SocketProgramming_java/UDPClient.java
+++ SocketProgramming_java/UDPClient.java
@@ -1,3 +1,8 @@
+/* Name:       GUO JIACHUN
+ * Group:      TS1
+ * IP Address: 192.168.22.1
+ */
+
 import java.net.*;
 import java.io.*;
 import java.util.Scanner;
@@ -16,20 +21,32 @@ public class UDPClient{
 			int portNo = Integer.valueOf(args[1]).intValue();
 			InetAddress host = InetAddress.getByName(args[0]);
 
-			while(true){
-				byte[] buffer = new byte[256];
-				System.out.print("Enter message to send: ");
-				String requestMessage = sc.nextLine();
-				DatagramPacket request = new DatagramPacket(requestMessage.getBytes(), requestMessage.length(), host, portNo);
-				socket.send(request);
+			// while(true){
+			// 	byte[] buffer = new byte[256];
+			// 	System.out.print("Enter message to send: ");
+			// 	String requestMessage = sc.nextLine();
+			// 	DatagramPacket request = new DatagramPacket(requestMessage.getBytes(), requestMessage.length(), host, portNo);
+			// 	socket.send(request);
 
-				DatagramPacket reply = new DatagramPacket(buffer, 256);
-				socket.receive(reply);
-				// remove extra empty characters
-				int i;
-				for(i=0; i<256 && buffer[i]!=0; i++);
-				System.out.println("Receive message: " + new String(buffer,0,i));
-			}
+			// 	DatagramPacket reply = new DatagramPacket(buffer, 256);
+			// 	socket.receive(reply);
+			// 	// remove extra empty characters
+			// 	int i;
+			// 	for(i=0; i<256 && buffer[i]!=0; i++);
+			// 	System.out.println("Receive message: " + new String(buffer,0,i));
+			// }
+
+			byte[] buffer = new byte[256];
+			String requestMessage = "GUO JIACHUN, TS1, 192.168.22.1";
+			DatagramPacket request = new DatagramPacket(requestMessage.getBytes(), requestMessage.length(), host, portNo);
+			socket.send(request);
+
+			DatagramPacket reply = new DatagramPacket(buffer, 256);
+			socket.receive(reply);
+			// remove extra empty characters
+			int i;
+			for(i=0; i<256 && buffer[i]!=0; i++);
+			System.out.println("Receive message: " + new String(buffer,0,i));
 		} catch(SocketException e) {
 			if(socket != null)
 				socket.close();
